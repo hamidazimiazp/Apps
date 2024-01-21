d07a8cb7a33a4375cf1b0057b4da7e594482eac7
hamidazimiazp <hamidazimi.azp@gmail.com>
2024-01-21 15:08:31 +0330

    add theme

---
+++ QRCode/QRCodeApp/config/colors.js
@@ -0,0 +1,11 @@
+export const theme = {
+  primary: "#11009E",
+  secondary: "#4942E4",
+  black: "#000000",
+  white: "#ffffff",
+  danger: "#ff0e0e",
+  success: "#4BB543",
+  textDark: "#313234",
+  textLight: "#CDCDCD",
+  background: "#F9F9FB",
+};
