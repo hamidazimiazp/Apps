e00b287f6f571ae47fb8492755cb30b7d52e0fb3
hamidazimiazp <hamidazimi.azp@gmail.com>
2024-01-25 15:05:58 +0330

    add delete api

--- QRCode/backend/A/base/views.py
+++ QRCode/backend/A/base/views.py
@@ -4,9 +4,17 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import QRCode
 from .serializers import QRCodeSerializer
-
+from django.http import Http404
 
 class QrcodeApi(APIView):
+    
+    def get_object(self, pk):
+        try:
+            return QRCode.objects.get(pk=pk)
+        except QRCode.DoesNotExist:
+            raise Http404
+
+        
     def get(self, request):
         codes = QRCode.objects.all()
         ser_data = QRCodeSerializer(instance=codes, many=True).data
@@ -20,3 +28,9 @@ class QrcodeApi(APIView):
             ser_data.save()
             return Response(ser_data.data, status=status.HTTP_201_CREATED)
         return Response(ser_data.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    
+    def delete(self, request, pk):
+        code = self.get_object(pk)
+        code.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
